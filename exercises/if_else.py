cf86addddefd2a75e028ebc28f049481cec5c431
tang.zhengliang <tang.zhengliang@ivis-sh.com>
2025-06-11 17:42:42 +0900

    if else elif

--- exercises/if_else.py
+++ exercises/if_else.py
@@ -15,12 +15,21 @@
 def check_grade(score):
     """
     根据分数返回等级
-    
+
     参数:
     - score: 学生分数，0-100的整数
-    
+
     返回:
     - 对应的等级：优秀、良好、中等、及格、不及格
     """
     # 请在下方编写代码
-    pass 
+    if score >= 90:
+        return "优秀"
+    elif (score < 90) and (score >= 80):
+        return "良好"
+    elif (score < 80) and (score >= 70):
+        return "中等"
+    elif (score < 70) and (score >= 60):
+        return "及格"
+    else:
+        return "不及格"
