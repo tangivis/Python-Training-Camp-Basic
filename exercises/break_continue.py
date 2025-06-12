794976f0569c11902b350d96ed28857c24062aca
tang.zhengliang <tang.zhengliang@ivis-sh.com>
2025-06-12 13:40:19 +0900

    break continue

--- exercises/break_continue.py
+++ exercises/break_continue.py
@@ -10,12 +10,25 @@
 def skip_multiples_of_three(n):
     """
     返回从1到n的所有非3的倍数的整数列表
-    
+
     参数:
     - n: 正整数上限
-    
+
     返回:
     - 从1到n中所有不是3的倍数的整数列表
     """
     # 请在下方编写代码
-    pass 
+    not3_multiple = []
+    i = 1;
+
+    while True:
+        if i > n:
+            break
+
+        if i % 3 == 0:
+            continue
+
+        not3_multiple.append(i)
+        i += 1
+
+    return not3_multiple
--- exercises/for_loop.py
+++ exercises/for_loop.py
@@ -10,12 +10,15 @@
 def sum_numbers(n):
     """
     计算从1到n的所有整数之和
-    
+
     参数:
     - n: 正整数
-    
+
     返回:
     - 从1到n的所有整数之和
     """
     # 请在下方编写代码
-    pass 
+    sum = 0
+    for num in range(1, n + 1):
+        sum = sum + num
+    return sum
