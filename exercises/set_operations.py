acfb1d05d445b2ab8c3638f3744b323e73bed02c
tang.zhengliang <tang.zhengliang@ivis-sh.com>
2025-06-15 01:15:54 +0900

    union, intersection, difference

--- exercises/set_operations.py
+++ exercises/set_operations.py
@@ -10,14 +10,21 @@
 def student_set_operations(set1, set2, operation):
     """
     对两个学生集合进行操作
-    
+
     参数:
     - set1: 第一个学生集合
     - set2: 第二个学生集合
     - operation: 操作类型 ("union", "intersection", "difference")
-    
+
     返回:
     - 集合操作的结果
     """
     # 请在下方编写代码
-    pass
+    if operation == "union":
+        return set1 | set2
+    elif operation == "intersection":
+        return set1 & set2
+    elif operation == "difference":
+        return set1 - set2
+    else:
+        return "Invalid operation"
